d41e7afbb446bbb88a6b3be244b076c2a63eb7a3
ceccoemi <cecchini.mle@gmail.com>
2022-07-03 12:46:33 +0200

    Bump to version 1.5

--- gpyocr/__init__.py
+++ gpyocr/__init__.py
@@ -3,13 +3,13 @@ gpyocr - Python wrapper to Tesseract OCR and Google Vision OCR
 """
 
 
-__version__ = "1.4"
+__version__ = "1.5"
 
 
 from ._gpyocr import (
     SUPPORTED_FORMATS,
-    get_tesseract_version,
-    tesseract_ocr,
     get_google_vision_version,
+    get_tesseract_version,
     google_vision_ocr,
+    tesseract_ocr,
 )
